b0a320f9a35c5f20e2504fd192c1b183a2643155
slicedearth <47583576+slicedearth@users.noreply.github.com>
2020-06-03 13:21:29 +1000

    Created Home, About and Contact
    Created home, about and contact components.

---
+++ client/src/components/About.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div>
+      <h1 className='is-size-2 has-text-weight-bold'>About</h1>
+    </div>
+  );
+};
+
+export default About;
---
+++ client/src/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h1 className='is-size-2 has-text-weight-bold has-text-centered'>
+        React App
+      </h1>
+    </div>
+  );
+};
+
+export default Home;
