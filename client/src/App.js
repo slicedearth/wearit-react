6e2d0976e2c46e5a9ec27feb7af361961fcb1067
slicedearth <47583576+slicedearth@users.noreply.github.com>
2020-06-14 00:34:31 +1000

    Created Footer
    Created Footer component and added to App.js.

--- client/src/App.js
+++ client/src/App.js
@@ -1,32 +1,32 @@
-// PACKAGE IMPORTS
+// THIRD PARTY IMPORTS
 import React, { Fragment } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 // CUSTOM IMPORTS
 import Home from './components/Home';
 import About from './components/About';
 import Contact from './components/Contact';
-import SMS from './components/SMSForm';
 import Newsletter from './components/Newsletter.js';
 import Status from './components/Status';
 import NotFound from './components/NotFound';
 import NavigationBar from './components/layout/NavigationBar';
+import Footer from './components/layout/Footer';
 
 const App = () => {
   return (
     <Fragment>
       <NavigationBar />
-      {/* <Jumbotron /> */}
+      {/* ROUTES */}
       <Router>
         <Switch>
           <Route exact path='/' component={Home}></Route>
           <Route exact path='/newsletter' component={Newsletter}></Route>
           <Route exact path='/about' component={About}></Route>
           <Route exact path='/contact' component={Contact}></Route>
-          <Route exact path='/sms' component={SMS}></Route>
           <Route exact path='/status' component={Status}></Route>
           <Route component={NotFound}></Route>
         </Switch>
       </Router>
+      <Footer />
     </Fragment>
   );
 };
---
+++ client/src/components/layout/Footer.js
@@ -0,0 +1,41 @@
+// THIRD PARTY IMPORTS
+import React from 'react';
+import { Footer as BulmaFooter, Content } from 'react-bulma-components';
+import styled from 'styled-components';
+// CUSTOM CSS FOR ANCHOR TAG
+const A = styled.a`
+  color: #bbb;
+  &:hover {
+    color: #00d1b2;
+  }
+`;
+const Footer = () => {
+  return (
+    <BulmaFooter>
+      <Content class='has-text-centered has-text-weight-semibold'>
+        <p>Website built with Bulma and React 2020</p>
+        <p>
+          Illustrations courtesy of{' '}
+          <A
+            href='https://undraw.co/'
+            rel='noopener noreferrer'
+            target='_blank'
+          >
+            unDraw
+          </A>
+        </p>
+        <p>
+          Patterns courtesy of{' '}
+          <A
+            href='http://heropatterns.com/'
+            rel='noopener noreferrer'
+            target='_blank'
+          >
+            Hero Patterns
+          </A>
+        </p>
+      </Content>
+    </BulmaFooter>
+  );
+};
+export default Footer;
